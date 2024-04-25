fd7e3664f62bf33ab0201ef9394f3d0825c35695
kojix2 <2xijok@gmail.com>
2024-04-25 15:56:21 +0900

    Add ad hoc methods

--- src/fastx.cr
+++ src/fastx.cr
@@ -32,4 +32,36 @@ module Fastx
       raise ArgumentError.new("Unknown format: #{filename}")
     end
   end
+
+  # Experimental
+  # FIXME: Other than ACGTN are replaced with N but FASTA format allows other characters
+
+  def self.normalize_base(c : UInt8) : UInt8
+    case c
+    when 65u8, 97u8  then 65u8 # A
+    when 67u8, 99u8  then 67u8 # C
+    when 71u8, 103u8 then 71u8 # G
+    when 84u8, 116u8 then 84u8 # T
+    when 78u8, 110u8 then 78u8 # N
+    else
+      STDERR.puts "'#{c.chr}' is replaced with 'N'"
+      78u8 # N
+    end
+  end
+
+  # Experimental
+
+  def self.normalize_sequence(sequence : IO::Memory | String) : Slice(UInt8)
+    sequence.to_slice.map do |c|
+      normalize_base(c)
+    end
+  end
+
+  # Experimental
+
+  def self.normalize_sequence(sequence : IO::Memory | String) : Slice
+    sequence.to_slice.map do |c|
+      yield normalize_base(c)
+    end
+  end
 end
