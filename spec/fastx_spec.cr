71d9c415d5df45c1c55723c881bda3b1bd82e5e9
kojix2 <2xijok@gmail.com>
2024-09-23 21:46:00 +0900

    Improved test

--- spec/fastx_spec.cr
+++ spec/fastx_spec.cr
@@ -18,4 +18,19 @@ describe Fastx do
     end
     reader.close
   end
+
+  it "should open a fastq file" do
+    reader = Fastx.open(Path[__DIR__, "fixtures/moo.fq"], "r")
+    c = 0
+    reader.as(Fastx::Fastq::Reader).each do |id, sequence, quality|
+      id.should eq ["chr1_106_509:0/1", "chr1_437_492:1/1"][c]
+      sequence.size.should eq 100
+      quality.size.should eq 100
+      s = sequence.to_s
+      s.should eq [FQ_SEQ_1, FQ_SEQ_2][c]
+      q = quality.to_s
+      q.should eq [FQ_QUAL_1, FQ_QUAL_2][c]
+      c += 1
+    end
+  end
 end
