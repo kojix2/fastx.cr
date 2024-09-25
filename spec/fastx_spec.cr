c6ee41d5bcefdd2be92a6f069debe68bd1d6acd9
kojix2 <2xijok@gmail.com>
2024-09-25 10:16:16 +0900

    Add tests

--- spec/fastx_spec.cr
+++ spec/fastx_spec.cr
@@ -19,6 +19,20 @@ describe Fastx do
     reader.close
   end
 
+  it "should open a fasta file with block" do
+    Fastx.open(Path[__DIR__, "fixtures/moo.fa"], "r") do |reader|
+      c = 0
+      reader.as(Fastx::Fasta::Reader).each do |name, sequence|
+        name.should eq ["chr1 1", "chr2 2"][c]
+        sequence.size.should eq [1000, 900][c]
+        s = sequence.to_s
+        s.starts_with?([CHR1_START, CHR2_START][c]).should be_true
+        s.ends_with?([CHR1_END, CHR2_END][c]).should be_true
+        c += 1
+      end
+    end
+  end
+
   it "should open a fastq file" do
     reader = Fastx.open(Path[__DIR__, "fixtures/moo.fq"], "r")
     c = 0
@@ -31,6 +45,22 @@ describe Fastx do
       q = quality.to_s
       q.should eq [FQ_QUAL_1, FQ_QUAL_2][c]
       c += 1
+    end
+  end
+
+  it "should open a fastq file with a block" do
+    Fastx.open(Path[__DIR__, "fixtures/moo.fq"], "r") do |reader|
+      c = 0
+      reader.as(Fastx::Fastq::Reader).each do |id, sequence, quality|
+        id.should eq ["chr1_106_509:0/1", "chr1_437_492:1/1"][c]
+        sequence.size.should eq 100
+        quality.size.should eq 100
+        s = sequence.to_s
+        s.should eq [FQ_SEQ_1, FQ_SEQ_2][c]
+        q = quality.to_s
+        q.should eq [FQ_QUAL_1, FQ_QUAL_2][c]
+        c += 1
+      end
     end
   end
 end
